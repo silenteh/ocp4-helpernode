21dce078a5e518abffb5eef78b4d19f789e97c67
root <root@localhost.localdomain>
2021-04-10 09:17:23 -0400

    Added dhcp v6

--- containers/dhcp/scripts/startup.sh
+++ containers/dhcp/scripts/startup.sh
@@ -5,7 +5,9 @@
 #
 ## Variables for DHCP
 dhcpConfig=/etc/dhcp/dhcpd.conf
+dhcp6Config=/etc/dhcp/dhcpd6.conf
 dhcpConfigTemplate=/usr/local/src/dhcpd.conf.j2
+dhcp6ConfigTemplate=/usr/local/src/dhcpd6.conf.j2
 helperPodYaml=/usr/local/src/helperpod.yaml
 ansibleLog=/var/log/helperpod_ansible_run.log
 
@@ -21,8 +23,24 @@ echo ${HELPERPOD_CONFIG_YAML} | base64 -d > ${helperPodYaml}
 ## Create dhcpd.conf based on the template and yaml passed in.
 ansible localhost -c local -e @${helperPodYaml} -m template -a "src=${dhcpConfigTemplate} dest=${dhcpConfig}" > ${ansibleLog} 2>&1
 
+## Create dhcpd6.conf
+ansible localhost -c local -e @${helperPodYaml} -m template -a "src=${dhcp6ConfigTemplate} dest=${dhcp6Config}" > ${ansibleLog} 2>&1
+
 #
 ## Test for the validity of the config file. Run the DHCP process if it passes
+if ! /usr/sbin/dhcpd -6 -t -cf ${dhcp6Config} ; then
+        echo "=========================="
+        echo "FATAL: Invalid DHCP6 config"
+        echo "=========================="
+        exit 254
+else
+        echo "========================"
+        echo "Starting DHCP6 service..."
+        echo "========================"
+        /usr/sbin/dhcpd -6 -cf ${dhcp6Config} -user dhcpd -group dhcpd --no-pid
+fi
+
+
 if ! /usr/sbin/dhcpd -t -cf ${dhcpConfig} ; then
 	echo "=========================="
 	echo "FATAL: Invalid DHCP config"
